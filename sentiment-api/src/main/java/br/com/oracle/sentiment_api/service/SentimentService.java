0d8b2a4eaa090f9481a16ff6078873b15c05af7c
Karina-IV <ivkarina1979@gmail.com>
2026-01-14 01:07:23 +0300

    Update SentimentService.java

--- sentiment-api/src/main/java/br/com/oracle/sentiment_api/service/SentimentService.java
+++ sentiment-api/src/main/java/br/com/oracle/sentiment_api/service/SentimentService.java
@@ -3,7 +3,7 @@ package br.com.oracle.sentiment_api.service;
 import br.com.oracle.sentiment_api.dto.SentimentRequest;
 import br.com.oracle.sentiment_api.dto.SentimentResponse;
 import jakarta.validation.constraints.NotNull;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,28 +14,26 @@ public class SentimentService {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    // quando rodar localmente
-    private final String DS_API_URL = "https://sentiment-api-java.onrender.com/predict";
+    @Value("${ml.api.url}")
+    private String mlApiUrl;
 
-    public SentimentResponse analyze( @NotNull SentimentRequest request) {
+    public SentimentResponse analyze(@NotNull SentimentRequest request) {
         Map<String, String> payload = Map.of("text", request.text());
 
         try {
-            System.out.println("Tentando conectar em: " + DS_API_URL); // Log de debug
+            System.out.println("Tentando conectar em: " + mlApiUrl);
 
             return restTemplate.postForObject(
-                    DS_API_URL,
+                    mlApiUrl,
                     payload,
                     SentimentResponse.class
             );
         } catch (Exception e) {
-            // Isso vai imprimir o erro REAL no console/logs da nuvem
             System.err.println("ERRO AO CONECTAR NA API PYTHON: " + e.getMessage());
             e.printStackTrace();
-
-            // Retorna um erro genérico ou relança para o Controller tratar
             throw new RuntimeException("Falha na comunicação com serviço de IA", e);
         }
     }
 }
+
 
